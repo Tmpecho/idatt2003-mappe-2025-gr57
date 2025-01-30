3f724c32c7677e85983fdbf45b2af13d5982e142
Eilif Hjermann Lindblad <eilif.hjermann@gmail.com>
2025-01-30 14:32:33 +0100

    WIP: Setup App class for JavaFX
    Trying to setup the App class to load JavaFX modules. Gets error "JavaFX runtime components are missing".

--- src/main/java/edu/idi/idatt/App.java
+++ src/main/java/edu/idi/idatt/App.java
@@ -3,8 +3,25 @@ package edu.idi.idatt;
 /**
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  @Override
+  public void start(Stage stage) {
+    Label label = new Label("Hello, JavaFX!");
+    StackPane root = new StackPane(label);
+    Scene scene = new Scene(root, 400, 300);
+    stage.setScene(scene);
+    stage.setTitle("JavaFX Maven App");
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
 }
