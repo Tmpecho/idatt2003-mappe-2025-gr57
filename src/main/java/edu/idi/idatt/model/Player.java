a1a271bed6788e8fd24db2d6dd34a81caed3b3a5
Eilif Hjermann Lindblad <eilifhl@stud.ntnu.no>
2025-02-12 11:05:23 +0100

    Add basic fields to Player
    Added class Player with basic fields position and name

---
+++ src/main/java/edu/idi/idatt/model/Player.java
@@ -0,0 +1,22 @@
+package edu.idi.idatt.model;
+
+public class Player {
+  private final String name;
+  private int position;
+
+  public Player(String name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void setPosition(int position) {
+  }
+}
