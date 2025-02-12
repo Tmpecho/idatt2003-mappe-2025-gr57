bb93e5aae4efc17975abf974e4030274e3d4048d
Tmpecho <TMPEcho@gmail.com>
2025-02-12 15:56:59 +0100

    Add roll dice button and implement dice rolling functionality in Game class

--- src/main/java/edu/ntnu/idi/idatt/boardgame/App.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/App.java
@@ -2,7 +2,7 @@ package edu.ntnu.idi.idatt.boardgame;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -10,7 +10,7 @@ public class App extends Application {
   public void start(Stage stage) {
     Game game = new Game();
 
-    AnchorPane root = new AnchorPane(game.getGameBoard());
+    VBox root = new VBox(game.getGameBoard(), game.getRollDiceButton());
     Scene scene = new Scene(root, 700, 800);
     stage.setScene(scene);
     stage.setTitle("JavaFX Maven App");
