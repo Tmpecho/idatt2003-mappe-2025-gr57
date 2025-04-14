a7b2c86904b218615dc1aca7260161c1a39278b7
Tmpecho <TMPEcho@gmail.com>
2025-04-14 09:16:00 +0200

    Remove unused import statement from SnakesAndLaddersTile.java

--- src/main/java/edu/ntnu/idi/idatt/boardgame/games/snakesAndLadders/domain/board/SnakesAndLaddersTile.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/games/snakesAndLadders/domain/board/SnakesAndLaddersTile.java
@@ -11,7 +11,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
