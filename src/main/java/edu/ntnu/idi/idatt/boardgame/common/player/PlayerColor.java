22a3a5f76881e1c0f873c53aa2812ff337d6a7c9
Tmpecho <TMPEcho@gmail.com>
2025-04-14 09:17:19 +0200

    Format PlayerColor.java, Tile.java and SnakesAndLaddersTile.java

--- src/main/java/edu/ntnu/idi/idatt/boardgame/common/player/PlayerColor.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/common/player/PlayerColor.java
@@ -4,21 +4,21 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public enum PlayerColor {
-	RED,
-	BLUE,
-	GREEN,
-	YELLOW,
-	ORANGE,
-	PURPLE;
+  RED,
+  BLUE,
+  GREEN,
+  YELLOW,
+  ORANGE,
+  PURPLE;
 
-	public static Paint mapToJavaFXColor(PlayerColor color) {
-		return switch (color) {
-			case RED -> Color.RED;
-			case BLUE -> Color.BLUE;
-			case GREEN -> Color.GREEN;
-			case YELLOW -> Color.YELLOW;
-			case ORANGE -> Color.ORANGE;
-			case PURPLE -> Color.PURPLE;
-		};
-	}
+  public static Paint mapToJavaFXColor(PlayerColor color) {
+    return switch (color) {
+      case RED -> Color.RED;
+      case BLUE -> Color.BLUE;
+      case GREEN -> Color.GREEN;
+      case YELLOW -> Color.YELLOW;
+      case ORANGE -> Color.ORANGE;
+      case PURPLE -> Color.PURPLE;
+    };
+  }
 }
