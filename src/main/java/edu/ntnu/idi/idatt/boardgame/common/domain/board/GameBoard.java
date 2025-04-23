c5b90d84ae30a07c0c1138397ef2f8dcc3d4d09d
Eilif Hjermann Lindblad <eilifhl@stud.ntnu.no>
2025-04-23 21:25:13 +0200

    Add JSON-serializers to SnakesAndLadders (#50)
    * Implementesave and load game in SnakesAndLaddersController
    
    Implemented save and load game in SnakesAndLaddersController with toJson functions in GSON. This is not recommended but works for now.
    
    * Use JsonObject and JsonArray, add optional name, color variables to JSON
    
    * Fix formatting
    
    * Implement FileChooser for saving and loading game state

--- src/main/java/edu/ntnu/idi/idatt/boardgame/common/domain/board/GameBoard.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/common/domain/board/GameBoard.java
@@ -1,12 +1,21 @@
 package edu.ntnu.idi.idatt.boardgame.common.domain.board;
 
-import javafx.scene.Node;
-import edu.ntnu.idi.idatt.boardgame.common.player.Player;
 import java.util.Map;
 
+import edu.ntnu.idi.idatt.boardgame.common.player.Player;
+import javafx.scene.Node;
+
 public interface GameBoard {
+
     void addPlayersToStart(Map<Integer, Player> players);
+
     void incrementPlayerPosition(Player player, int increment);
+
     int getBoardSize();
+
+    Node getNode();
+
     void setView(Node view);
+
+    void setPlayerPosition(Player player, int position);
 }
