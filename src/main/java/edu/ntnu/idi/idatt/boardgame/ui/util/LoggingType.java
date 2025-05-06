177f6253ed2ec895a7815ba8be8ce1a25c2e234d
Tmpecho <TMPEcho@gmail.com>
2025-05-06 16:14:59 +0200

    Add logging notification utility for toast-style messages

---
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/ui/util/LoggingType.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idi.idatt.boardgame.ui.util;
+
+/**
+ * Different types of logging messages:
+ *
+ * <p>{@code INFO}, {@code DEBUG}, {@code ERROR}, {@code WARN}, {@code FATAL}
+ */
+public enum LoggingType {
+  INFO,
+  DEBUG,
+  ERROR,
+  WARN,
+  FATAL,
+}
