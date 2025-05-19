3a1c36dfd8e8b12be8c8cbef3a9020e9097db96e
Tmpecho <TMPEcho@gmail.com>
2025-05-19 10:25:38 +0200

    Format tests and simplify one assertion

--- src/test/java/edu/ntnu/idi/idatt/boardgame/core/domain/player/PlayerTest.java
+++ src/test/java/edu/ntnu/idi/idatt/boardgame/core/domain/player/PlayerTest.java
@@ -1,82 +1,83 @@
 package edu.ntnu.idi.idatt.boardgame.core.domain.player;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
-    private Player<LinearPos> linearPlayer;
-    private Player<GridPos> gridPlayer;
-    private final LinearPos initialLinearPos = new LinearPos(1);
-    private final GridPos initialGridPos = new GridPos(0, 0);
+  private final LinearPos initialLinearPos = new LinearPos(1);
+  private final GridPos initialGridPos = new GridPos(0, 0);
+  private Player<LinearPos> linearPlayer;
+  private Player<GridPos> gridPlayer;
 
-    @BeforeEach
-    void setUp() {
-        linearPlayer = new Player<>(1, "Alice", PlayerColor.RED, initialLinearPos);
-        gridPlayer = new Player<>(2, "Bob", PlayerColor.BLUE, initialGridPos);
-    }
+  @BeforeEach
+  void setUp() {
+    linearPlayer = new Player<>(1, "Alice", PlayerColor.RED, initialLinearPos);
+    gridPlayer = new Player<>(2, "Bob", PlayerColor.BLUE, initialGridPos);
+  }
 
-    @Test
-    void constructor_setsFieldsCorrectly_forLinearPos() {
-        assertEquals(1, linearPlayer.getId());
-        assertEquals("Alice", linearPlayer.getName());
-        assertEquals(PlayerColor.RED, linearPlayer.getColor());
-        assertEquals(initialLinearPos, linearPlayer.getPosition());
-    }
+  @Test
+  void constructor_setsFieldsCorrectly_forLinearPos() {
+    assertEquals(1, linearPlayer.getId());
+    assertEquals("Alice", linearPlayer.getName());
+    assertEquals(PlayerColor.RED, linearPlayer.getColor());
+    assertEquals(initialLinearPos, linearPlayer.getPosition());
+  }
 
-    @Test
-    void constructor_setsFieldsCorrectly_forGridPos() {
-        assertEquals(2, gridPlayer.getId());
-        assertEquals("Bob", gridPlayer.getName());
-        assertEquals(PlayerColor.BLUE, gridPlayer.getColor());
-        assertEquals(initialGridPos, gridPlayer.getPosition());
-    }
+  @Test
+  void constructor_setsFieldsCorrectly_forGridPos() {
+    assertEquals(2, gridPlayer.getId());
+    assertEquals("Bob", gridPlayer.getName());
+    assertEquals(PlayerColor.BLUE, gridPlayer.getColor());
+    assertEquals(initialGridPos, gridPlayer.getPosition());
+  }
 
-    @Test
-    void setPosition_updatesPosition_forLinearPos() {
-        LinearPos newPos = new LinearPos(10);
-        linearPlayer.setPosition(newPos);
-        assertEquals(newPos, linearPlayer.getPosition());
-    }
+  @Test
+  void setPosition_updatesPosition_forLinearPos() {
+    LinearPos newPos = new LinearPos(10);
+    linearPlayer.setPosition(newPos);
+    assertEquals(newPos, linearPlayer.getPosition());
+  }
 
-    @Test
-    void setPosition_updatesPosition_forGridPos() {
-        GridPos newPos = new GridPos(5, 5);
-        gridPlayer.setPosition(newPos);
-        assertEquals(newPos, gridPlayer.getPosition());
-    }
+  @Test
+  void setPosition_updatesPosition_forGridPos() {
+    GridPos newPos = new GridPos(5, 5);
+    gridPlayer.setPosition(newPos);
+    assertEquals(newPos, gridPlayer.getPosition());
+  }
 
-    @Test
-    void getPosition_returnsCurrentPosition() {
-        assertEquals(initialLinearPos, linearPlayer.getPosition());
-        assertEquals(initialGridPos, gridPlayer.getPosition());
-    }
+  @Test
+  void getPosition_returnsCurrentPosition() {
+    assertEquals(initialLinearPos, linearPlayer.getPosition());
+    assertEquals(initialGridPos, gridPlayer.getPosition());
+  }
 
-    @Test
-    void getId_returnsCorrectId() {
-        assertEquals(1, linearPlayer.getId());
-        assertEquals(2, gridPlayer.getId());
-    }
+  @Test
+  void getId_returnsCorrectId() {
+    assertEquals(1, linearPlayer.getId());
+    assertEquals(2, gridPlayer.getId());
+  }
 
-    @Test
-    void getName_returnsCorrectName() {
-        assertEquals("Alice", linearPlayer.getName());
-        assertEquals("Bob", gridPlayer.getName());
-    }
+  @Test
+  void getName_returnsCorrectName() {
+    assertEquals("Alice", linearPlayer.getName());
+    assertEquals("Bob", gridPlayer.getName());
+  }
 
-    @Test
-    void getColor_returnsCorrectColor() {
-        assertEquals(PlayerColor.RED, linearPlayer.getColor());
-        assertEquals(PlayerColor.BLUE, gridPlayer.getColor());
-    }
+  @Test
+  void getColor_returnsCorrectColor() {
+    assertEquals(PlayerColor.RED, linearPlayer.getColor());
+    assertEquals(PlayerColor.BLUE, gridPlayer.getColor());
+  }
 
-    @Test
-    void toString_returnsExpectedFormat() {
-        String expectedLinear = "Player{id=1, name=Alice, position=index: 1, color=RED}";
-        assertEquals(expectedLinear, linearPlayer.toString());
+  @Test
+  void toString_returnsExpectedFormat() {
+    String expectedLinear = "Player{id=1, name=Alice, position=index: 1, color=RED}";
+    assertEquals(expectedLinear, linearPlayer.toString());
 
-        String expectedGrid = "Player{id=2, name=Bob, position=[0, 0], color=BLUE}";
-        assertEquals(expectedGrid, gridPlayer.toString());
-    }
+    String expectedGrid = "Player{id=2, name=Bob, position=[0, 0], color=BLUE}";
+    assertEquals(expectedGrid, gridPlayer.toString());
+  }
 }
--- src/test/java/edu/ntnu/idi/idatt/boardgame/ui/util/PlayerColorMapperTest.java
+++ src/test/java/edu/ntnu/idi/idatt/boardgame/ui/util/PlayerColorMapperTest.java
@@ -1,48 +1,48 @@
 package edu.ntnu.idi.idatt.boardgame.ui.util;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import edu.ntnu.idi.idatt.boardgame.core.domain.player.PlayerColor;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(ApplicationExtension.class)
 class PlayerColorMapperTest {
 
-    @Test
-    void toPaint_returnsCorrectPaintForWhite() {
-        assertEquals(Color.WHITE, PlayerColorMapper.toPaint(PlayerColor.WHITE));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForWhite() {
+    assertEquals(Color.WHITE, PlayerColorMapper.toPaint(PlayerColor.WHITE));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForRed() {
-        assertEquals(Color.RED, PlayerColorMapper.toPaint(PlayerColor.RED));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForRed() {
+    assertEquals(Color.RED, PlayerColorMapper.toPaint(PlayerColor.RED));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForBlue() {
-        assertEquals(Color.BLUE, PlayerColorMapper.toPaint(PlayerColor.BLUE));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForBlue() {
+    assertEquals(Color.BLUE, PlayerColorMapper.toPaint(PlayerColor.BLUE));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForGreen() {
-        assertEquals(Color.GREEN, PlayerColorMapper.toPaint(PlayerColor.GREEN));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForGreen() {
+    assertEquals(Color.GREEN, PlayerColorMapper.toPaint(PlayerColor.GREEN));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForYellow() {
-        assertEquals(Color.YELLOW, PlayerColorMapper.toPaint(PlayerColor.YELLOW));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForYellow() {
+    assertEquals(Color.YELLOW, PlayerColorMapper.toPaint(PlayerColor.YELLOW));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForOrange() {
-        assertEquals(Color.ORANGE, PlayerColorMapper.toPaint(PlayerColor.ORANGE));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForOrange() {
+    assertEquals(Color.ORANGE, PlayerColorMapper.toPaint(PlayerColor.ORANGE));
+  }
 
-    @Test
-    void toPaint_returnsCorrectPaintForPurple() {
-        assertEquals(Color.PURPLE, PlayerColorMapper.toPaint(PlayerColor.PURPLE));
-    }
+  @Test
+  void toPaint_returnsCorrectPaintForPurple() {
+    assertEquals(Color.PURPLE, PlayerColorMapper.toPaint(PlayerColor.PURPLE));
+  }
 }
