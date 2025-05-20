6206c1ce33497849275a5d1926e0726ddc874583
Tmpecho <TMPEcho@gmail.com>
2025-05-20 18:24:40 +0200

    Remove generic `incrementPlayerPosition` method
    Eliminated `incrementPlayerPosition` from `GameBoard` and irrelevant implementations in Cluedo. Updated Snakes and Ladders to use `movePlayer` for improved clarity and action specificity.

--- src/main/java/edu/ntnu/idi/idatt/boardgame/core/domain/board/GameBoard.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/core/domain/board/GameBoard.java
@@ -1,15 +1,12 @@
 package edu.ntnu.idi.idatt.boardgame.core.domain.board;
 
-import java.util.Map;
-
 import edu.ntnu.idi.idatt.boardgame.core.domain.player.Player;
 import edu.ntnu.idi.idatt.boardgame.core.domain.player.Position;
+import java.util.Map;
 
 public interface GameBoard<P extends Position> {
 
   void addPlayersToStart(Map<Integer, Player<P>> players);
-
-  void incrementPlayerPosition(Player<P> player, int increment);
 
   int getBoardSize();
 
--- src/main/java/edu/ntnu/idi/idatt/boardgame/games/snakesandladders/engine/action/RollAction.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/games/snakesandladders/engine/action/RollAction.java
@@ -1,17 +1,17 @@
 package edu.ntnu.idi.idatt.boardgame.games.snakesandladders.engine.action;
 
-import edu.ntnu.idi.idatt.boardgame.core.domain.board.GameBoard;
 import edu.ntnu.idi.idatt.boardgame.core.domain.dice.Dice;
+import edu.ntnu.idi.idatt.boardgame.core.domain.player.LinearPos;
 import edu.ntnu.idi.idatt.boardgame.core.domain.player.Player;
-import edu.ntnu.idi.idatt.boardgame.core.domain.player.Position;
 import edu.ntnu.idi.idatt.boardgame.core.engine.action.Action;
+import edu.ntnu.idi.idatt.boardgame.games.snakesandladders.domain.board.SnLBoard;
 
-public final class RollAction<P extends Position> implements Action {
-  private final GameBoard<P> gameBoard;
-  private final Player<P> player;
+public final class RollAction implements Action {
+  private final SnLBoard gameBoard;
+  private final Player<LinearPos> player;
   private final Dice dice;
 
-  public RollAction(GameBoard<P> gameBoard, Player<P> player, Dice dice) {
+  public RollAction(SnLBoard gameBoard, Player<LinearPos> player, Dice dice) {
     this.gameBoard = gameBoard;
     this.player = player;
     this.dice = dice;
