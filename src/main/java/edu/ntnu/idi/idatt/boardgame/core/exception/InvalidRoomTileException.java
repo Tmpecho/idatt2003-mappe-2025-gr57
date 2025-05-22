3a5625924daeb007d076b77020539a9570fdaac0
Eilif Hjermann Lindblad <eilifhl@stud.ntnu.no>
2025-05-22 16:19:37 +0200

    Formatted and added javadocs

--- src/main/java/edu/ntnu/idi/idatt/boardgame/core/exception/InvalidRoomTileException.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/core/exception/InvalidRoomTileException.java
@@ -1,11 +1,26 @@
 package edu.ntnu.idi.idatt.boardgame.core.exception;
 
+/**
+ * Exception thrown when an unknown player color is encountered. This exception is a runtime
+ * exception and does not require explicit handling.
+ */
 public class InvalidRoomTileException extends RuntimeException {
 
+  /**
+   * Constructs a new InvalidRoomTileException with the specified detail message.
+   *
+   * @param message the detail message
+   */
   public InvalidRoomTileException(String message) {
     super(message);
   }
 
+  /**
+   * Constructs a new InvalidRoomTileException with the specified detail message and cause.
+   *
+   * @param message the detail message
+   * @param cause   the cause of the exception
+   */
   public InvalidRoomTileException(String message, Throwable cause) {
     super(message, cause);
   }
--- src/main/java/edu/ntnu/idi/idatt/boardgame/core/exception/InvalidTileException.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/core/exception/InvalidTileException.java
@@ -1,11 +1,27 @@
 package edu.ntnu.idi.idatt.boardgame.core.exception;
 
+/**
+ * Exception thrown when an unknown player color is encountered. This exception is a runtime
+ * exception and does not require explicit handling.
+ */
 public class InvalidTileException extends RuntimeException {
-    public InvalidTileException(String message) {
-        super(message);
-    }
 
-    public InvalidTileException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  /**
+   * Constructs a new InvalidTileException with the specified detail message.
+   *
+   * @param message the detail message
+   */
+  public InvalidTileException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new InvalidTileException with the specified detail message and cause.
+   *
+   * @param message the detail message
+   * @param cause   the cause of the exception
+   */
+  public InvalidTileException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
