41b2bbbac43936cb184cfbd27b84b6de756eb16d
Eilif Hjermann Lindblad <eilifhl@stud.ntnu.no>
2025-05-22 16:14:22 +0200

    Fix tests and add InvalidRoomException

---
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/core/exception/InvalidRoomException.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idi.idatt.boardgame.core.exception;
+
+public class InvalidRoomException extends RuntimeException {
+
+  public InvalidRoomException(String message) {
+    super(message);
+  }
+
+  public InvalidRoomException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
